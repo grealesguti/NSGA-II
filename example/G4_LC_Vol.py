9377b696b267c7f7520b0524ad50951c5254d7b6
grealesguti <g.realesguti@gmail.com>
2022-11-08 22:35:10 +0100

    add: example for G4 with Volume objective
    Look for maximum LC and minimum Volume frontier

---
+++ example/G4_LC_Vol.py
@@ -0,0 +1,35 @@
+from nsga2.problem import Problem
+from nsga2.evolution import Evolution
+import matplotlib.pyplot as plt
+import math
+import ROOT
+
+def f1(indv, generation, PopName="Generation_",Pop=0,Folder="../../../Results/"):
+    f=PopName+str(generation)+"_"+str(indv)+"_0.root"
+    inFile = ROOT . TFile . Open ( Folder+f ," READ ")
+    tree = inFile . Get ("EndOfRun")
+    tree.GetEntry(0)
+    LC = getattr(tree,"fLCAvg")
+    inFile.Close()
+    return -1*LC
+
+def f2(indv, generation, PopName="Generation_",Pop=0,Folder="../../../Results/"):
+    f=PopName+str(generation)+"_"+str(indv)+"_0.root"
+    inFile = ROOT . TFile . Open ( Folder+f ," READ ")
+    tree = inFile . Get ("EndOfRun")
+    tree.GetEntry(0)
+    Vol = getattr(tree,"fVolume")
+    inFile.Close()
+    return Vol
+
+nindv=100
+problem = Problem(num_of_variables=10, objectives=[f1,f2], variables_range=[(0.5,1.75)], same_range=True, expand=False, obj_idx=True)
+evo = Evolution(problem, mutation_param=20, num_of_generations=100, num_of_individuals=100, TierII=1)
+
+indv=[i for i in evo.evolve()]
+func = [i.objectives for i in indv]
+feat = [i.features for i in indv]
+
+print(func)
+print(feat)
+
