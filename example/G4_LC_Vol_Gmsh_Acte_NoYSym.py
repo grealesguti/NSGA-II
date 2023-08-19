d37604cf7295d0cc045d20fd80ab05cb4145b224
grealesguti <g.realesguti@gmail.com>
2023-08-19 01:53:23 -0700

    add: NoYSym script

--- example/G4_LC_Vol_Gmsh_Acte_NoYSym.py
+++ example/G4_LC_Vol_Gmsh_Acte_NoYSym.py
@@ -4,6 +4,7 @@ from nsga2.evolution import Evolution
 import math
 import ROOT
 from nsga2.G4Inp import G4Inp
+import subprocess
 
 G4input = G4Inp(CurrentFolder="/storage/af/user/greales/simG4/BTL_LYSOARRAY_LO_G4/", OutFolder =  "/storage/af/user/greales/simG4/outputs/", SubName="SubDefaultName", OutName="Out_NSGA", JobName="JobActionNSGATestGmsh.sh", SiPMS=False, LYSOL=True, ROOTName='Generation_', RelativeFolder="../../../Results/", Singularity = '+SingularityImage="/storage/af/user/greales/SingDir/sandG4Gmsh"',SZloc=False)
 '''
@@ -45,11 +46,12 @@ def f1(indv, generation, PopName=G4input.ROOTName,Pop=0,Folder=G4input.RelativeF
         inFile = ROOT . TFile . Open ( Folder+f ," READ ")
         tree = inFile . Get ("EndOfRun")
         tree.GetEntry(0)
-        LC = getattr(tree,"fLCAvg")
+        LC = getattr(tree,"fLCP50")
         inFile.Close()
     except Exception as e:
         print("Exception in LC gen. ",str(generation)," indv. ",str(indv))
         LC = -1e9
+        p = subprocess.call(['condor_rm',"greales"])
     return -1*LC
 
 def f2(indv, generation, PopName=G4input.ROOTName,Pop=0,Folder=G4input.RelativeFolder):
@@ -60,9 +62,11 @@ def f2(indv, generation, PopName=G4input.ROOTName,Pop=0,Folder=G4input.RelativeF
         tree.GetEntry(0)
         Vol = getattr(tree,"fVolume")
         inFile.Close()
+        
     except Exception as e:
         print("Exception in Vol gen. ",str(generation)," indv. ",str(indv))
         Vol = 1e9
+        p = subprocess.call(['condor_rm',"greales"])
     return Vol
 
 def f1v2(individual_id, generation_num, Population_name=G4input.ROOTName,Population = 0, ROOT_Folder=G4input.RelativeFolder, tree_name="EndOfRun", variable_name="fLCAvg",exception_return=0):
@@ -96,6 +100,9 @@ def f2v2(individual_id, generation_num, Population_name=G4input.ROOTName,Populat
 n_of_variables=5*2+1
 
 var_range=[(18.5,28.5),(0.25,2.25),(0.25,2.25),(0.25,2.25),(0.25,2.25),(0.25,2.25),(0.25,2.25),(0.25,2.25),(0.25,2.25),(0.25,2.25),(0.25,2.25)]
+#var_range=[(18.5,28.5),(0.75,2.25),(0.75,2.25),(0.75,2.25),(0.75,2.25),(0.75,2.25),(0.75,2.25),(0.75,2.25),(0.75,2.25),(0.75,2.25),(0.75,2.25)]
+
+
 
 problem = Problem(num_of_variables=n_of_variables, objectives=[f1,f2], variables_range=var_range, same_range=False, expand=False, obj_idx=True)
 evo = Evolution(problem,G4InputClass=G4input, mutation_param=20, num_of_generations=400, num_of_individuals=100, TierII=1,init_evo='',Generation_evo=0 )
